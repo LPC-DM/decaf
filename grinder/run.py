43620702c9370fa71079bd263e20595c6e996899
Matteo Cremonesi <matteoc@mac-132345.guest-network.net>
2019-06-12 15:17:42 -0500

    first commit for processor work

--- grinder/run.py
+++ grinder/run.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python
-from __future__ import print_function, division
-from collections import defaultdict, OrderedDict
-import warnings
-import concurrent.futures
-import gzip
+import lz4.frame as lz4f
 import pickle
 import json
 import time
-import numexpr
-import os
+import cloudpickle
 from optparse import OptionParser
-import uproot, uproot_methods
+
+import uproot
 import numpy as np
-from fnal_column_analysis_tools import hist
-from analysis.darkhiggs import analysis,samples
+from coffea import hist, processor
+from analysis.darkhiggs import Processor,samples
 
 parser = OptionParser()
 parser.add_option('-d', '--dataset', help='dataset', dest='dataset')
 parser.add_option('-y', '--year', help='year', dest='year')
-parser.add_option('-l', '--lumi', help='lumi', dest='lumi')
+parser.add_option('-l', '--lumi', help='lumi', dest='lumi', type=float)
+parser.add_option('-w', '--workers', help='Number of workers to use for multi-worker executors (e.g. futures or condor)', dest='workers', type=int, default=8)
 (options, args) = parser.parse_args()
-
-with open("../harvester/beans/"+options.year+".json") as fin:
-    datadef = json.load(fin)
-
-hists = {}
-dataset_xs = {k: v['xs'] for k,v in datadef.items()}
 
 lumis = {}
 #Values from https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmVAnalysisSummaryTable
@@ -33,68 +24,45 @@ lumis['2016']=35.92
 lumis['2017']=41.53
 lumis['2018']=59.97
 lumi = 1000.*float(lumis[options.year])
-if options.lumi: lumi=1000.*float(options.lumi)
-print(lumi)
+if options.lumi: lumi=1000.*options.lumi
 
-tstart = time.time()
-for dataset, info in datadef.items():
-    nevents = 0
-    sumw = 0
-    selections = []
+with open("../harvester/beans/"+options.year+".json") as fin:
+    samplefiles = json.load(fin)
+    
+xsec = {k: v['xs'] for k,v in samplefiles.items()}
+filelist = []
+selections = []
+fileslice = slice(None)
+
+for dataset, info in samplefiles.items():
     if options.dataset and options.dataset not in dataset: continue
+    for file in info['files'][fileslice]:
+        filelist.append((dataset, file))
     for selection,v in samples.items():
-        #if options.selection and options.selection not in selection: continue    
         for i in range (0,len(v)):
             if v[i] not in dataset: continue
             selections.append(selection)
-        print(dataset,selections)
-        ## futures.update(exector.submit(function, item) for item in items)
-        # So need to put selections, options.year, dataset_xs, dataset, and file in an items dict
-        futures.update(executor.submit(analysis, selections, options.year, dataset_xs[dataset], dataset, file) for file in info['files'][fileslice]) ##
-        if(len(futures)==0): continue ##
-        try:
-            total = len(futures)
-            processed = 0
-            while len(futures) > 0: ##
-                finished = set(job for job in futures if job.done()) ##
-                for job in finished: ##
-                    #accumulator += job.result() <- this is only hout for the processor framework. Need another way to get the other three
-                    dataset, sumws, nentries, hout = job.result()
-                    nevents += nentries
-                    sumw += sumws
-                    for k in hout.keys():
-                        if k in hists: hists[k] += hout[k]
-                        else: hists[k]= hout[k]
-                    print("Processing: done with % 4d / % 4d files" % (processed, total))
-                    # Might be replaced with pbar, whateve rthat is?
-                    processed += 1
-                futures -= finished ##
-            del finished ##
-        except KeyboardInterrupt: ##
-            print("Ok quitter") ##
-            for job in futures: job.cancel() ##
-        except: ## 
-            for job in futures: job.cancel() ##
-            raise ##
 
-        print(dataset,"nevents:",nevents,"sumw:",sumw,"sumw from hist:",hists['sumw'].values(overflow='all')[(dataset,)][1])
-        scale = 1
-        if dataset_xs[dataset]!= -1: scale = lumi*dataset_xs[dataset]# / sumw
-        print("xsec:",dataset_xs[dataset],"xsec weight:",scale)
-        for key in hists.keys():
-            if key=='sumw': continue
-            hists[key].scale(scale)
-        print('after scaling:',hists['sumw'].values(overflow='all')[(dataset,)][1])
-        dt = time.time() - tstart
+processor_instance=Processor(selected_regions=selections, year=options.year, xsec=xsec, lumi=lumi)
+tstart = time.time()
+output = processor.run_uproot_job(filelist,
+                                  treename='Events',
+                                  processor_instance=processor_instance,
+                                  executor=processor.futures_executor,
+                                  executor_args={'workers': options.workers},
+                                  chunksize=500000,
+                                  )
+processor_instance.postprocess(output)
+    
+nbins = sum(sum(arr.size for arr in h._sumw.values()) for h in final_accumulator.values() if isinstance(h, hist.Hist))
+nfilled = sum(sum(np.sum(arr > 0) for arr in h._sumw.values()) for h in final_accumulator.values() if isinstance(h, hist.Hist))
+print("Filled %.1fM bins" % (nbins/1e6, ))
+print("Nonzero bins: %.1f%%" % (100*nfilled/nbins, ))
+
+# Pickle is not very fast or memory efficient, will be replaced by something better soon
+with lz4f.open(args.output, mode="wb", compression_level=5) as fout:
+    cloudpickle.dump(output, fout)
         
-        print("%.2f us*cpu/event" % (1e6*dt*nworkers/nevents, ))
-        nbins = sum(sum(arr.size for arr in h._sumw.values()) for h in hists.values())
-        nfilled = sum(sum(np.sum(arr>0) for arr in h._sumw.values()) for h in hists.values())
-        print("Processed %.1fM events" % (nevents/1e6, ))
-        print("Filled %.1f bins" % nbins)
-        print("Nonzero bins: %.1f%%" % (100*nfilled/nbins, ))
-        os.system("mkdir -p pods/"+options.year+"/"+selection)
-        # Pickle is not very fast or memory efficient, will be replaced by something better soon
-        with gzip.open("pods/"+options.year+"/"+dataset+".pkl.gz", "wb") as fout:
-            pickle.dump(hists, fout)
-
+dt = time.time() - tstart
+nworkers = args.workers
+print("%.2f us*cpu overall" % (1e6*dt*nworkers, ))
