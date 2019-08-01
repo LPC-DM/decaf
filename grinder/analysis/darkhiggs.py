dc3b0a8a46de19bbca42dcf04b1ff2f63bf2e9f0
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2019-08-01 14:28:06 -0500

    new cuts

--- grinder/analysis/darkhiggs.py
+++ grinder/analysis/darkhiggs.py
@@ -436,10 +436,10 @@ class AnalysisProcessor(processor.ProcessorABC):
             selections.add('istwoM', (e_nloose==0) & (mu_ntight==1) & (mu_nloose==2) & (tau_nloose==0)&(pho_nloose==0)&(leading_dimu.mass.sum()>60) & (leading_dimu.mass.sum()<120))
             selections.add('istwoE', (e_ntight==1)&(e_nloose==2)&(mu_nloose==0)&(tau_nloose==0)&(pho_nloose==0)&(leading_diele.mass.sum()>60)&(leading_diele.mass.sum()<120))
             selections.add('isoneA', (e_nloose==0)&(mu_nloose==0)&(tau_nloose==0)&(pho_ntight==1))
-            selections.add('topveto', (j_ndflvL==0)&(leading_fj.TopTagger.sum()<0.15))
-            selections.add('ismonohs', (leading_fj.DarkHiggsTagger.sum()>0.1))
-            selections.add('ismonoV', ~(leading_fj.DarkHiggsTagger.sum()>0.1)&(leading_fj.VvsQCDTagger.sum()>0.8))
-            selections.add('ismonojet', ~(leading_fj.DarkHiggsTagger.sum()>0.1)&~(leading_fj.VvsQCDTagger.sum()>0.8))
+            selections.add('topveto', (j_ndflvL==0)&(leading_fj.TopTagger.sum()<0.25))
+            selections.add('ismonohs', (leading_fj.DarkHiggsTagger.sum()>0.2))
+            selections.add('ismonoV', ~(leading_fj.DarkHiggsTagger.sum()>0.2)&(leading_fj.VvsQCDTagger.sum()>0.8))
+            selections.add('ismonojet', ~(leading_fj.DarkHiggsTagger.sum()>0.2)&~(leading_fj.VvsQCDTagger.sum()>0.8))
 
             ###
             #Adding weights and selections
