c407660f7a81bd8761130a05141b85e56051c242
matteoc <matteoc@popeye-login2.cm.cluster>
2019-08-02 12:05:31 -0700

    fixing mess

---
+++ grinder/cmd_parsl.auto.1564772661.3928525.sh
@@ -0,0 +1,1 @@
+process_worker_pool.py  --max_workers=48 -p 0 -c 1.0 -m None --poll 10 --task_url=tcp://popeye-login2:54965 --result_url=tcp://popeye-login2:54870 --logdir=/home/matteoc/decaf/grinder/runinfo/001/coffea_parsl_slurm --block_id=0 --hb_period=30 --hb_threshold=120 
