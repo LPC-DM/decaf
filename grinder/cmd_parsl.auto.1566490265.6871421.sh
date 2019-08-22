43c75602c33e724d3ee3c37613d0837829347de9
matteoc <matteoc@popeye-login1.cm.cluster>
2019-08-22 09:27:00 -0700

    some minor progress on popeye

---
+++ grinder/cmd_parsl.auto.1566490265.6871421.sh
@@ -0,0 +1,1 @@
+process_worker_pool.py  --max_workers=48 -p 0 -c 1.0 -m None --poll 10 --task_url=tcp://popeye-login2:54768 --result_url=tcp://popeye-login2:54771 --logdir=/home/matteoc/decaf/grinder/runinfo/002/coffea_parsl_slurm --block_id=10 --hb_period=30 --hb_threshold=120 
