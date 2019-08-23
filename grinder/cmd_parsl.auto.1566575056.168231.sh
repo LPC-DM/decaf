b8037fd362c44e016e5303635da92efc1e082498
matteoc <matteoc@popeye-login1.cm.cluster>
2019-08-23 08:46:53 -0700

    trying to fix xrootd issue

---
+++ grinder/cmd_parsl.auto.1566575056.168231.sh
@@ -0,0 +1,1 @@
+process_worker_pool.py  --max_workers=48 -p 0 -c 1.0 -m None --poll 10 --task_url=tcp://popeye-login1:54513 --result_url=tcp://popeye-login1:54384 --logdir=/home/matteoc/decaf/grinder/runinfo/000/coffea_parsl_slurm --block_id=65 --hb_period=30 --hb_threshold=120 
