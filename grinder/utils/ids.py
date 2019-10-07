f86ea577a2d12b71a3e92ad8a604010895be325c
Quantumapple <jongho.lee@cern.ch>
2019-10-07 13:27:52 -0500

    remove jet id cut

--- grinder/utils/ids.py
+++ grinder/utils/ids.py
@@ -183,5 +183,5 @@ def isGoodJet(pt, eta, jet_id, nhf, nef, chf, cef):
     return mask
 
 def isHEMJet(pt, eta, phi, jet_id, nhf, nef, chf, cef):
-    mask = (pt>30) & ((eta>-2.4)&(eta<-1.4)) & ((phi>-1.57)&(phi<-0.87)) & (nhf<0.8) & (nef<0.99) & (chf>0.1) & (cef<0.99)
+    mask = (pt>30) & ((eta>-2.4)&(eta<-1.4)) & ((phi>-1.57)&(phi<-0.87))
     return mask
