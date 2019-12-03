5331c0fe92a4c1450d426c364ca7478013293af1
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2019-12-03 15:23:42 -0600

    starting the work on limits

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,9 +1,9 @@
 # http://lcginfo.cern.ch/release/95apython3/
 # Try to guess SL6 vs. CC7
 if uname -r | grep -q el6; then
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-slc6-gcc8-opt/setup.sh
+  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-slc6-gcc8-opt/setup.sh
 else
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh
+  source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
 fi
 
 export PYTHONPATH=~/.local/lib/python3.6/site-packages:$PYTHONPATH
