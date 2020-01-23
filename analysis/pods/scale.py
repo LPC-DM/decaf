abb75d1443629646122f6cb70203eaf7b0c1d053
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2020-01-23 14:25:41 -0600

    rewriting scaling

--- analysis/pods/scale.py
+++ analysis/pods/scale.py
@@ -3,27 +3,40 @@ import gzip
 import os
 from collections import defaultdict, OrderedDict
 from coffea import hist, processor 
-from coffea.util import save
+from coffea.util import load, save
 
-def scale(folder):
-    ###
-    #
-    # Loading MC histograms
-    #
-    ###
 
-    mc_hists={}
+
+def scale_file(file):
+
+    #hists={}
     pd = []
-    dirname = folder
-    for filename in os.listdir(dirname):
-        if 'MET' in filename or 'SingleElectron' in filename or 'SinglePhoton' in filename or 'EGamma' in filename: continue
+
+    hists = load(file)
+    for d in hists['sumw'].identifiers('dataset'):
+        dataset = d.name
+        #if 'MET' in dataset or 'SingleElectron' in dataset or 'SinglePhoton' in dataset or 'EGamma' in dataset: continue
+        if dataset.split("____")[0] not in pd: pd.append(dataset.split("____")[0])
+
+    return scale(pd, hists)
+
+def scale_directory(directory):
+
+    hists={}
+    pd = []
+    for filename in os.listdir(directory):
         if '.pkl.gz' in filename:
             if filename.split("____")[0] not in pd: pd.append(filename.split("____")[0])
-            with gzip.open(dirname+'/'+filename) as fin:
+            with gzip.open(directory+'/'+filename) as fin:
                 hin = pickle.load(fin)
                 for k in hin.keys():
-                    if k in mc_hists: mc_hists[k]+=hin[k]
-                    else: mc_hists[k]=hin[k]
+                    if k in hists: hists[k]+=hin[k]
+                    else: hists[k]=hin[k]
+    
+    return scale(pd, hists)
+
+def scale(pd, hists):
+
     ##
     # Defining primary datasets (pdataset) to aggregate all the histograms that belong to a single sample
     ##
@@ -33,20 +46,21 @@ def scale(folder):
     pdataset_map = OrderedDict()
     for pdi in pd:
         pdataset_map[pdi] = (pdi+"*",)
-    for key in mc_hists.keys():
-        mc_hists[key] = mc_hists[key].group(pdataset_cats, pdataset, pdataset_map)
+    for key in hists.keys():
+        hists[key] = hists[key].group(pdataset_cats, pdataset, pdataset_map)
 
     ###
     # Rescaling MC histograms using the xsec weight
     ###
 
     scale={}
-    for pdi in mc_hists['sumw'].identifiers('pdataset'):
-        scale[pdi]=mc_hists['sumw'].integrate('pdataset', pdi).values(overflow='all')[()][1]
-    for key in mc_hists.keys():
+    for pdi in hists['sumw'].identifiers('pdataset'):
+        scale[pdi]=hists['sumw'].integrate('pdataset', pdi).values(overflow='all')[()][1]
+    for key in hists.keys():
         if key=='sumw': continue
-        for pdi in mc_hists[key].identifiers('pdataset'):
-            mc_hists[key].scale({pdi:1/scale[pdi]},axis='pdataset')
+        for pdi in hists[key].identifiers('pdataset'):
+            if 'MET' in pdi.name or 'SingleElectron' in pdi.name or 'SinglePhoton' in pdi.name or 'EGamma' in pdi.name: continue
+            hists[key].scale({pdi:1/scale[pdi]},axis='pdataset')
 
     ###
     # Defining 'process', to aggregate different samples into a single process
@@ -75,49 +89,40 @@ def scale(folder):
     signal_map["MonoZ"] = ("MonoZ*",)    ## signals
     signal_hists = {}
 
-    ###
-    # Storing signal and background histograms
-    ###
-
-    for key in mc_hists.keys():
-        signal_hists[key] = mc_hists[key].group(signal_cats, process, signal_map)
-        bkg_hists[key] = mc_hists[key].group(bkg_cats, process, bkg_map)
-
-    ###
-    #
-    # Loading data histograms
-    #
-    ###
-
-    data_hists={}
-    for filename in os.listdir(dirname):
-        if 'MET' in filename or 'SingleElectron' in filename or 'SinglePhoton' in filename or 'EGamma' in filename:
-            if '.pkl.gz' in filename:
-                with gzip.open(dirname+'/'+filename) as fin:
-                    hin = pickle.load(fin)
-                    for k in hin.keys():
-                        if k in data_hists: data_hists[k]+=hin[k]
-                        else: data_hists[k]=hin[k]
-
+    data_cats = ("pdataset",)
     data_map = OrderedDict()
     data_map["MET"] = ("MET*", )
     data_map["SingleElectron"] = ("EGamma*", )
     data_map["SinglePhoton"] = ("EGamma*", )
-    data_cats = ("dataset",)
-    for key in data_hists.keys():
-        data_hists[key] = data_hists[key].group(data_cats, process, data_map)
+    data_hists = {}
+
+    ###
+    # Storing signal and background histograms
+    ###
+
+    for key in hists.keys():
+        signal_hists[key] = hists[key].group(signal_cats, process, signal_map)
+        bkg_hists[key] = hists[key].group(bkg_cats, process, bkg_map)
+        data_hists[key] = hists[key].group(data_cats, process, data_map)
 
     return signal_hists, bkg_hists, data_hists
 
 if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser()
-    parser.add_option('-f', '--folder', help='folder', dest='folder')
+    parser.add_option('-f', '--file', help='file', dest='file')
+    parser.add_option('-d', '--directory', help='directory', dest='directory')
     (options, args) = parser.parse_args()
 
-    signal_hists, bkg_hists, data_hists = scale(options.folder)
+    if options.directory: 
+        signal_hists, bkg_hists, data_hists = scale_directory(options.directory)
+        name = options.directory
+    if options.file: 
+        signal_hists, bkg_hists, data_hists = scale_file(options.file)
+        name = options.file.split(".")[0].split("_")[1]        
+
     hists={}
     hists['signal']=signal_hists
     hists['bkg']=bkg_hists
     hists['data']=data_hists
-    save(hists,'hists_'+options.folder+'.coffea')
+    save(hists,'hists_'+name+'.coffea')
