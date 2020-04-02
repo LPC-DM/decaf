56941e3cf964c6ef34322a204b869d4366a0f2a9
Matteo Cremonesi <matteoc@ui20.sdfarm.kr>
2020-04-03 02:12:49 +0900

    improving merging

--- analysis/merge.py
+++ analysis/merge.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import cloudpickle
 import pickle
 import gzip
@@ -15,6 +16,30 @@ def split(arr, size):
      arrs.append(arr)
      return arrs
 
+def merging(mergingfile,filelist):
+     print('Generating merged file:',mergingfile+'.merged') 
+     hists={}
+     for filename in filelist:
+          fin = folder+'/'+filename
+          print('Opening:',fin)
+          hin = load(fin)
+          for k in hin.keys():
+               if k not in hists: hists[k]=hin[k]
+               else: hists[k]+=hin[k]
+          #fin.close()
+          del hin
+     dataset = hist.Cat("dataset", "dataset", sorting='placement')
+     dataset_cats = ("dataset",)
+     dataset_map = OrderedDict()
+     for d in hists[list(hists.keys())[0]].identifiers('dataset'):
+          new_dname = d.name.split("____")[0] + '____' + mergingfile.split("____")[1]
+          print("Merging",d.name,"into",new_dname)
+          if new_dname not in dataset_map:
+               dataset_map[new_dname] = (d.name.split("____")[0]+"*",)
+     for key in hists.keys():
+          hists[key] = hists[key].group(dataset_cats, dataset, dataset_map)
+     return hists
+
 def merge(folder,_dataset=None):
 
      pd = []
@@ -23,42 +48,40 @@ def merge(folder,_dataset=None):
                if filename.split("____")[0] not in pd: pd.append(filename.split("____")[0])
      print('List of primary datasets:',pd)
 
-     filelist={}
-     for pdi in pd:
-          files = []
-          for filename in os.listdir(folder):
-               if '.futures' not in filename: continue
-               if pdi not in filename: continue
-               files.append(filename)
-          split_files=split(files, 100)
-          for i in range(0,len(split_files)) :
-               filelist[pdi+'____'+str(i)+'_']=split_files[i]
-
-     for mergingfile in filelist.keys():
-          if _dataset is not None and _dataset not in mergingfile: continue
-          print('Generating merged file:',mergingfile+'.merged') 
-          hists={}
-          for filename in filelist[mergingfile]:
-               fin = folder+'/'+filename
-               print('Opening:',fin)
-               hin = load(fin)
-               for k in hin.keys():
-                    if k not in hists: hists[k]=hin[k]
-                    else: hists[k]+=hin[k]
-               #fin.close()
-               del hin
-          dataset = hist.Cat("dataset", "dataset", sorting='placement')
-          dataset_cats = ("dataset",)
-          dataset_map = OrderedDict()
-          for d in hists[list(hists.keys())[0]].identifiers('dataset'):
-               new_dname = d.name.split("____")[0] + '____' + mergingfile.split("____")[1]
-               print("Merging",d.name,"into",new_dname)
-               if new_dname not in dataset_map:
-                    dataset_map[new_dname] = (d.name.split("____")[0]+"*",)
-          for key in hists.keys():
-               hists[key] = hists[key].group(dataset_cats, dataset, dataset_map)
-          save(hists,folder+'/'+mergingfile+'.merged')
-          del hists
+     with concurrent.futures.ProcessPoolExecutor(max_workers=16) as executor:
+          futures = set()
+          for pdi in pd:
+               if _dataset is not None and _dataset not in pdi: continue
+               files = []
+               for filename in os.listdir(folder):
+                    if '.futures' not in filename: continue
+                    if pdi not in filename: continue
+                    files.append(filename)
+               split_files=split(files, int(len(files)/8) )
+               for i in range(0,len(split_files)) :
+                    mergingfile=pdi+'____'+str(i)+'_'
+                    filelist=split_files[i]
+                    futures.update(executor.submit(merging,mergingfile,filelist))
+                    if(len(futures)==0): continue
+                    try:
+                         total = len(futures)
+                         processed = 0
+                         while len(futures) > 0:
+                              finished = set(job for job in futures if job.done())
+                              for job in finished:
+                                   hists = job.result()
+                                   save(hists,folder+'/'+mergingfile+'.merged')
+                                   del hists
+                                   print("Processing: done with % 4d / % 4d files" % (processed, total))
+                                   processed += 1
+                              futures -= finished
+                         del finished
+                    except KeyboardInterrupt:
+                         print("Ok quitter")
+                         for job in futures: job.cancel()
+                    except:
+                         for job in futures: job.cancel()
+                         raise
 
      mergedlist=[]
      for mergedfile in os.listdir(folder):
