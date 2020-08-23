b3b956781daeae0964351c8567d4eb052836cbeb
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2020-08-23 02:52:34 -0500

    fixed weights

--- analysis/processors/darkhiggs.py
+++ analysis/processors/darkhiggs.py
@@ -1041,25 +1041,25 @@ class AnalysisProcessor(processor.ProcessorABC):
             nnlo_nlo = {}
             if('GJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['a']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['a'][systematic](genIsoAs.pt.max())*((genIsoAs.counts>0)&(genIsoAs.pt>=100)) + \
-                                           get_nnlo_nlo_weight['a'][systematic](100)*((genIsoAs.counts>0)&~(genIsoAs.pt>=100)) + \
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['a'][systematic](genIsoAs.pt.max())*((genIsoAs.counts>0)&(genIsoAs.pt.max()>=100)) + \
+                                           get_nnlo_nlo_weight['a'][systematic](100)*((genIsoAs.counts>0)&~(genIsoAs.pt.max()>=100)) + \
                                            (~(genIsoAs.counts>0)).astype(np.int)
             elif('WJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['w']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['w'][systematic](genWs.pt.max())*((genWs.counts>0)&(genWs.pt>=100)) + \
-                                           get_nnlo_nlo_weight['w'][systematic](100)*((genWs.counts>0)&~(genWs.pt>=100)) + \
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['w'][systematic](genWs.pt.max())*((genWs.counts>0)&(genWs.pt.max()>=100)) + \
+                                           get_nnlo_nlo_weight['w'][systematic](100)*((genWs.counts>0)&~(genWs.pt.max()>=100)) + \
                                            (~(genWs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['WJets'][self._year]
             elif('DY' in dataset): 
                 for systematic in get_nnlo_nlo_weight['dy']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['dy'][systematic](genDYs.pt.max())*((genDYs.counts>0)&(genDYs.pt>=100)) + \
-                                           get_nnlo_nlo_weight['dy'][systematic](100)*((genDYs.counts>0)&~(genDYs.pt>=100)) + \
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['dy'][systematic](genDYs.pt.max())*((genDYs.counts>0)&(genDYs.pt.max()>=100)) + \
+                                           get_nnlo_nlo_weight['dy'][systematic](100)*((genDYs.counts>0)&~(genDYs.pt.max()>=100)) + \
                                            (~(genDYs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['DY'][self._year]
             elif('ZJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['z']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['z'][systematic](genZs.pt.max())*((genZs.counts>0)&(genZs.pt>=100)) + \
-                                           get_nnlo_nlo_weight['z'][systematic](100)*((genZs.counts>0)&~(genZs.pt>=100)) + \
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['z'][systematic](genZs.pt.max())*((genZs.counts>0)&(genZs.pt.max()>=100)) + \
+                                           get_nnlo_nlo_weight['z'][systematic](100)*((genZs.counts>0)&~(genZs.pt.max()>=100)) + \
                                            (~(genZs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['ZJets'][self._year]
 
