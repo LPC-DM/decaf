757ae062378dbab1758ae2948447af846955fdd7
Matteo Cremonesi <matteoc@ui20.sdfarm.kr>
2020-08-24 12:51:25 +0900

    new scaled 2018

--- analysis/processors/darkhiggs.py
+++ analysis/processors/darkhiggs.py
@@ -1042,25 +1042,29 @@ class AnalysisProcessor(processor.ProcessorABC):
             if('GJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['a']:
                     nnlo_nlo[systematic] = get_nnlo_nlo_weight['a'][systematic](genIsoAs.pt.max())*((genIsoAs.counts>0)&(genIsoAs.pt.max()>=290)) + \
-                                           get_nnlo_nlo_weight['a'][systematic](290)*((genIsoAs.counts>0)&~(genIsoAs.pt.max()>=290)) + \
-                                           (~(genIsoAs.counts>0)).astype(np.int)
+                                           (~((genIsoAs.counts>0)&(genIsoAs.pt.max()>=290))).astype(np.int)
+                                           #get_nnlo_nlo_weight['a'][systematic](290)*((genIsoAs.counts>0)&~(genIsoAs.pt.max()>=290)) + \
+                                           #(~(genIsoAs.counts>0)).astype(np.int)
             elif('WJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['w']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['w'][systematic](genWs.pt.max())*((genWs.counts>0)&(genWs.pt.max()>=100)) + \
-                                           get_nnlo_nlo_weight['w'][systematic](100)*((genWs.counts>0)&~(genWs.pt.max()>=100)) + \
-                                           (~(genWs.counts>0)).astype(np.int)
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['w'][systematic](genWs.pt.max())*((genWs.counts>0)&(genWs.pt.max()>=30)) + \
+                                           (~((genWs.counts>0)&(genWs.pt.max()>=30))).astype(np.int)
+                                           #get_nnlo_nlo_weight['w'][systematic](100)*((genWs.counts>0)&~(genWs.pt.max()>=100)) + \
+                                           #(~(genWs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['WJets'][self._year]
             elif('DY' in dataset): 
                 for systematic in get_nnlo_nlo_weight['dy']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['dy'][systematic](genDYs.pt.max())*((genDYs.counts>0)&(genDYs.pt.max()>=100)) + \
-                                           get_nnlo_nlo_weight['dy'][systematic](100)*((genDYs.counts>0)&~(genDYs.pt.max()>=100)) + \
-                                           (~(genDYs.counts>0)).astype(np.int)
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['dy'][systematic](genDYs.pt.max())*((genDYs.counts>0)&(genDYs.pt.max()>=30)) + \
+                                           (~((genDYs.counts>0)&(genDYs.pt.max()>=30))).astype(np.int)
+                                           #get_nnlo_nlo_weight['dy'][systematic](100)*((genDYs.counts>0)&~(genDYs.pt.max()>=100)) + \
+                                           #(~(genDYs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['DY'][self._year]
             elif('ZJets' in dataset): 
                 for systematic in get_nnlo_nlo_weight['z']:
-                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['z'][systematic](genZs.pt.max())*((genZs.counts>0)&(genZs.pt.max()>=100)) + \
-                                           get_nnlo_nlo_weight['z'][systematic](100)*((genZs.counts>0)&~(genZs.pt.max()>=100)) + \
-                                           (~(genZs.counts>0)).astype(np.int)
+                    nnlo_nlo[systematic] = get_nnlo_nlo_weight['z'][systematic](genZs.pt.max())*((genZs.counts>0)&(genZs.pt.max()>=30)) + \
+                                           (~((genZs.counts>0)&(genZs.pt.max()>=30))).astype(np.int)
+                                           #get_nnlo_nlo_weight['z'][systematic](100)*((genZs.counts>0)&~(genZs.pt.max()>=100)) + \
+                                           #(~(genZs.counts>0)).astype(np.int)
                     #nnlo_nlo[systematic]=nnlo_nlo[systematic]*kfactor['ZJets'][self._year]
 
             ###
