39fab15032a04c329c9384788bb5ba0f82043dfd
Matteo Cremonesi <matteoc@fnal.gov>
2020-12-28 19:24:57 -0600

    still adjusting tt model

--- analysis/models/darkhiggs.py
+++ analysis/models/darkhiggs.py
@@ -171,7 +171,7 @@ def initialize_nuisances(hists, year):
                 0.0001,
                 0.9999,
                 )
-            for i in range(sr_ttFail.size())
+            for i in range(sr_ttFail.shape)
             ]
         )
         sr_ttNuisances[recoilbin]["pass"] = eff_tt * sr_ttNuisances[recoilbin]["fail"]
