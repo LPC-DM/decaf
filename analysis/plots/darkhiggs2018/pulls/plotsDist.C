d6f3928776b0837058c47866f88f6eaf590f49e3
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2021-01-13 05:10:50 +0900

    adding pulls

--- analysis/plots/darkhiggs2018/pulls/plotsDist.C
+++ analysis/plots/darkhiggs2018/pulls/plotsDist.C
@@ -1,7 +1,7 @@
 void plotsDist()
 {
 //=========Macro generated from canvas: asdf/asdf
-//=========  (Sun Jan 10 10:06:56 2021) by ROOT version 6.12/07
+//=========  (Wed Jan 13 03:59:58 2021) by ROOT version 6.12/07
    TCanvas *asdf = new TCanvas("asdf", "asdf",0,0,800,800);
    gStyle->SetOptFit(1);
    asdf->SetHighLightColor(2);
@@ -14,25 +14,25 @@ void plotsDist()
    
    TH1F *pulls__1 = new TH1F("pulls__1","",60,-2,2);
    pulls__1->SetBinContent(0,2);
-   pulls__1->SetBinContent(9,2);
-   pulls__1->SetBinContent(18,1);
-   pulls__1->SetBinContent(23,1);
+   pulls__1->SetBinContent(8,2);
+   pulls__1->SetBinContent(21,1);
+   pulls__1->SetBinContent(22,1);
    pulls__1->SetBinContent(24,1);
    pulls__1->SetBinContent(25,1);
-   pulls__1->SetBinContent(27,3);
-   pulls__1->SetBinContent(29,2);
-   pulls__1->SetBinContent(30,2);
-   pulls__1->SetBinContent(31,3);
-   pulls__1->SetBinContent(32,1);
+   pulls__1->SetBinContent(26,2);
+   pulls__1->SetBinContent(27,1);
+   pulls__1->SetBinContent(28,1);
+   pulls__1->SetBinContent(29,3);
+   pulls__1->SetBinContent(30,3);
+   pulls__1->SetBinContent(32,2);
    pulls__1->SetBinContent(33,1);
-   pulls__1->SetBinContent(34,1);
+   pulls__1->SetBinContent(34,2);
    pulls__1->SetBinContent(35,1);
-   pulls__1->SetBinContent(36,1);
+   pulls__1->SetBinContent(36,2);
    pulls__1->SetBinContent(37,1);
-   pulls__1->SetBinContent(38,3);
+   pulls__1->SetBinContent(38,1);
    pulls__1->SetBinContent(39,2);
-   pulls__1->SetBinContent(44,1);
-   pulls__1->SetBinContent(50,1);
+   pulls__1->SetBinContent(40,1);
    pulls__1->SetBinContent(61,1);
    pulls__1->SetEntries(32);
    pulls__1->SetStats(0);
@@ -41,17 +41,17 @@ void plotsDist()
    gaus1->SetFillColor(19);
    gaus1->SetFillStyle(0);
    gaus1->SetLineWidth(3);
-   gaus1->SetChisquare(4.861075);
+   gaus1->SetChisquare(4.397062);
    gaus1->SetNDF(16);
-   gaus1->SetParameter(0,1.474605);
-   gaus1->SetParError(0,1.608511);
+   gaus1->SetParameter(0,1.328635);
+   gaus1->SetParError(0,0.6281243);
    gaus1->SetParLimits(0,0,0);
-   gaus1->SetParameter(1,-3.105426);
-   gaus1->SetParError(1,11.87369);
+   gaus1->SetParameter(1,-1.112949);
+   gaus1->SetParError(1,10.91497);
    gaus1->SetParLimits(1,0,0);
-   gaus1->SetParameter(2,5.857344);
-   gaus1->SetParError(2,3.284432);
-   gaus1->SetParLimits(2,0,5.857515);
+   gaus1->SetParameter(2,5.15978);
+   gaus1->SetParError(2,4.323747);
+   gaus1->SetParLimits(2,0,5.15978);
    gaus1->SetParent(pulls__1);
    pulls__1->GetListOfFunctions()->Add(gaus1);
    pulls__1->SetMarkerStyle(20);
