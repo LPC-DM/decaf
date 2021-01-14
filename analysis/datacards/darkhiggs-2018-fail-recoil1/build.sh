22299ff3eb8c9df0ec8369d792fdc4029c2f3935
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2021-01-14 17:05:01 +0900

    prefit plots

--- analysis/datacards/darkhiggs-2018-fail-recoil1/build.sh
+++ analysis/datacards/darkhiggs-2018-fail-recoil1/build.sh
@@ -1,2 +1,2 @@
-combineCards.py wecr2018failrecoil1=wecr2018failrecoil1.txt > wecr2018failrecoil1_combined.txt
-text2workspace.py wecr2018failrecoil1_combined.txt
+combineCards.py wmcr2018failrecoil1=wmcr2018failrecoil1.txt > wmcr2018failrecoil1_combined.txt
+text2workspace.py wmcr2018failrecoil1_combined.txt
