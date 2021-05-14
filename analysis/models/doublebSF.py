84083829ddf0560a49bd9a21f16561bf6fd6b222
Quantumapple <jongapples@gmail.com>
2021-05-15 08:35:44 +0900

    add template function

--- analysis/models/doublebSF.py
+++ analysis/models/doublebSF.py
@@ -20,6 +20,25 @@ rl.ParametericSample.PreferRooParametricHist = False
 category_map = {"pass": 1, "fail": 0}
 gentype_map = ['bb', 'b', 'cc', 'c', 'other']
 
+def template(dictionary, process, gentype, category, read_sumw2=False):
+    histogram = dictionary[gentype].integrate("process", process)
+    jp, sumw2 = histogram.values(sumw2=True)[()]
+    jp = jp[category_map[category]]
+    sumw2 = sumw2[category_map[category]]
+    zerobins = jp <= 0.
+    output = jp
+
+    output[zerobins] = 1.
+    sumw2[zerobins] = 0.
+
+    binning = (
+        dictionary[gentype].integrate("process", process).axis("btagJP").edges()
+    )
+
+    if read_sumw2:
+        return (output, binning, "btagJP", sumw2)
+    return (output, binning, "btagJP")
+
 if __name__ == "__main__":
     if not os.path.exists("datacards"):
         os.mkdir("datacards")
@@ -42,14 +61,17 @@ if __name__ == "__main__":
     ##
 
     data = {}
-    for r in data_hists["template"].identifiers("process"):
-        data[str(r)] = data_hists["template"].sum("process", "gentype", overflow='all')
+    data['bb'] = data_hists["template"].sum("gentype", overflow='all')
 
     background = {}
     signal = {}
 
     for i in range(5):
         if gentype_map[i] == 'bb':
-            signal[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", 0).sum("process")
+            signal[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", 0)
         else:
-            background[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", i).sum("process")
+            background[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", i)
+
+    print(data)
+    print(signal)
+    print(background)
