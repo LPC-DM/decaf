7808a4b9d3c314fe43514908fa3f494af6216b8a
Quantumapple <jongapples@gmail.com>
2021-06-16 15:32:55 +0900

    remove old files

--- analysis/datacards/doublebSF-2018-fail/build.sh
+++
@@ -1,2 +0,0 @@
-combineCards.py btagJP2018fail=btagJP2018fail.txt > model_combined.txt
-text2workspace.py model_combined.txt
--- analysis/datacards/doublebSF-2018-pass/build.sh
+++
@@ -1,2 +0,0 @@
-combineCards.py btagJP2018pass=btagJP2018pass.txt > model_combined.txt
-text2workspace.py model_combined.txt
