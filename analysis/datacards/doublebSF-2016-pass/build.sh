4b52dc6a9aaf44fa03ad25ba16e14d98bd5ca86d
Quantumapple <jongapples@gmail.com>
2021-06-18 13:31:41 +0900

    delete old datacards

--- analysis/datacards/doublebSF-2016-pass/build.sh
+++
@@ -1,2 +0,0 @@
-combineCards.py btagJP2016pass=btagJP2016pass.txt > model_combined.txt
-text2workspace.py model_combined.txt
--- analysis/datacards/doublebSF-2017-fail/build.sh
+++
@@ -1,2 +0,0 @@
-combineCards.py btagJP2017fail=btagJP2017fail.txt > model_combined.txt
-text2workspace.py model_combined.txt
