5114198efffa5500ad1663c48cd22edbabf1bd33
jongho <jongho@pop-os.localdomain>
2022-07-31 20:44:34 -0500

    temporary

---
+++ analysis/combine_condor_v2.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+from collections import defaultdict, OrderedDict
+import warnings
+import concurrent.futures
+import gzip
+import pickle
+import json
+import time
+import numexpr
+import os
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-a', '--analysis', help='analysis', dest='analysis', default='')
+parser.add_option('-c', '--cluster', help='cluster', dest='cluster', default='lpc')
+parser.add_option('-t', '--tar', action='store_true', dest='tar')
+parser.add_option('-x', '--copy', action='store_true', dest='copy')
+parser.add_option('-m', '--method', help='combine method', dest='method', default='fit')
+(options, args) = parser.parse_args()
+
+#for folder in os.listdir('datacards/'):
+#    if options.analysis not in folder: continue
+#    if '-' in folder: continue
+#    os.system('rm -rf datacards/'+folder+'/condor/combine/err/*')
+#    os.system('rm -rf datacards/'+folder+'/condor/combine/log/*')
+#    os.system('rm -rf datacards/'+folder+'/condor/combine/out/*')
+
+if options.tar:
+    os.system('tar --exclude-caches-all --exclude-vcs --exclude=\'../../../CMSSW_10_2_13/src/decaf/datacards-*\' --exclude=\'../../../CMSSW_10_2_13/src/decaf/data\' --exclude=\'../../../CMSSW_10_2_13/src/decaf/plots\' --exclude=\'../../../CMSSW_10_2_13/src/decaf/datacards/*/condor\' -czvf ../../../cmssw.tgz  ../../../CMSSW_10_2_13')
+
+if options.cluster == 'kisti':
+    if options.copy:
+        os.system('xrdfs root://cms-xrdr.private.lo:2094/ rm /xrd/store/user/'+os.environ['USER']+'/cmssw.tgz')
+        print('cmssw removed')
+        os.system('xrdcp -f ../../../cmssw.tgz root://cms-xrdr.private.lo:2094//xrd/store/user/'+os.environ['USER']+'/cmssw.tgz')
+        print('cmssw copied!')
+    jdl = """universe = vanilla
+Executable = combine.sh
+Should_Transfer_Files = YES
+WhenToTransferOutput = ON_EXIT
+Transfer_Input_Files = combine.sh, /tmp/x509up_u556950775
+Output = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/out/$ENV(METHOD)_$(Cluster)_$(Process).stdout
+Error = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/err/$ENV(METHOD)_$(Cluster)_$(Process).stderr
+Log = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/log/$ENV(FOLDER)_$(Cluster)_$(Process).log
+Arguments = $ENV(FOLDER) $ENV(CLUSTER) $ENV(USER) $ENV(METHOD)
+accounting_group=group_cms
+JobBatchName = fit
+request_disk = 9000
+Queue 1"""
+#TransferOutputRemaps = "fitDiagnostics.root=$ENV(PWD)/datacards/$ENV(FOLDER)/fitDiagnostics.root"
+
+if options.cluster == 'lpc':
+    os.system('xrdcp -f ../../../cmssw.tgz root://cmseos.fnal.gov//store/user/'+os.environ['USER']+'/cmssw.tgz')
+    jdl = """universe = vanilla
+Executable = combine.sh
+Should_Transfer_Files = YES
+WhenToTransferOutput = ON_EXIT
+Transfer_Input_Files = combine.sh
+Output = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/out/$ENV(METHOD)_$(Cluster)_$(Process).stdout
+Error = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/err/$ENV(METHOD)_$(Cluster)_$(Process).stderr
+Log = datacards/$ENV(FOLDER)/condor/$ENV(METHOD)/log/$ENV(FOLDER)_$(Cluster)_$(Process).log
+Arguments = $ENV(FOLDER) $ENV(CLUSTER) $ENV(USER) $ENV(METHOD)
+JobBatchName = fit
+request_disk = 9000
+request_memory = 8000
+Queue 1"""
+
+jdl_file = open("combine.submit", "w")
+jdl_file.write(jdl)
+jdl_file.close()
+
+for folder in os.listdir('datacards/'):
+    if options.analysis not in folder: continue
+    if '-' in folder: continue
+    print('Preparing job for folder', folder)
+    os.system('mkdir -p datacards/'+folder+'/condor/'+options.method+'/err/')
+    os.system('mkdir -p datacards/'+folder+'/condor/'+options.method+'/log/')
+    os.system('mkdir -p datacards/'+folder+'/condor/'+options.method+'/out/')
+    os.environ['FOLDER']   = folder
+    os.environ['CLUSTER'] = options.cluster
+    os.environ['METHOD'] = options.method
+    os.system('condor_submit combine.submit')
+os.system('rm combine.submit')
