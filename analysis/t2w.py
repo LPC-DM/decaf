04689ccbb64213e76b076f7e2231b1ee4c387e59
Matteo Cremonesi <matteoc@fnal.gov>
2022-09-01 12:31:28 -0700

    Update t2w.py

--- analysis/t2w.py
+++ analysis/t2w.py
@@ -9,9 +9,19 @@ if __name__ == '__main__':
     parser.add_option('-d', '--datacard', help='datacard', dest='datacard')
     (options, args) = parser.parse_args()
     
+    signals=[]
     for k,v in processes.items():
         process = k
         if not isinstance(k, str):
             process = k[0]
         if options.signal not in process: continue
-        print(process)
+        signals.append(process)
+    
+    tmp = []
+    for signal in signals:
+        label = ' --PO map=.*/'+signal+':r_'+signal+'[1,0,2]'
+        tmp.append(label)
+    option = ''.join(tmp)
+    
+    command = "text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose"+option+' '+options.datacard+' -o '+options.datacard.split(.)[0]+'.root'
+    print(command, '\n')
