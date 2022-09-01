535acdcba31d7ba626b62b7cd47ab89474a5c9b3
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2022-09-02 06:27:48 +0900

    still developing t2w.py

--- analysis/t2w.py
+++ analysis/t2w.py
@@ -6,7 +6,7 @@ from data.process import *
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-s', '--signal', help='signal', dest='signal')
-    parser.add_option('-d', '--datacard', help='datacard', dest='datacard')
+    parser.add_option('-f', '--folder', help='folder', dest='folder')
     (options, args) = parser.parse_args()
     
     signals=[]
@@ -18,17 +18,19 @@ if __name__ == '__main__':
         signals.append(process)
     
     tmp = []
+    name = ''
     for signal in signals:
         try:
             if options.signal.split(':')[1] in signal:
-                label = ' --PO map=.*/'+signal+':'+signal+'_r[1,0,10]'
+                name='_'+signal.replace('_','')
+                label = ' --PO \'map=.*/'+signal+':'+signal+'_r[1,0,10]\''
             else:
-                label = ' --PO map=.*/'+signal+':0'
+                label = ' --PO \'map=.*/'+signal+':0\''
         except:
-            label = ' --PO map=.*/'+signal+':'+signal+'_r[1,0,10]'
+            label = ' --PO \'map=.*/'+signal+':'+signal+'_r[1,0,10]\''
         tmp.append(label)
     option = ''.join(tmp)
     
-    command = "text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose"+option+' '+options.datacard+' -o '+options.datacard.split('.')[0]+'.root'
-    #print(command)
-    os.system(command)
+    command = "text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose"+option+' datacards/'+options.folder+'/'+options.folder+'.txt -o datacards/'+options.folder+'/'+options.folder+name+'.root'
+    print(command)
+    #os.system(command)
