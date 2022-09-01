2e010dcba1f5f74c3711396ef419f7c6b579a885
Matteo Cremonesi <matteoc@fnal.gov>
2022-09-01 11:07:13 -0700

    Update combine_cards.py

--- analysis/macros/combine_cards.py
+++ analysis/macros/combine_cards.py
@@ -6,10 +6,10 @@ parser.add_option('-a', '--analysis', help='analysis', dest='analysis', default=
 (options, args) = parser.parse_args()
 
 folder_list = []
-for tgz in os.listdir('datacards/'):
-    if '.tgz' in tgz: continue
-    if 'condor' in tgz: continue
-    folder_list.append(tgz.split('.')[0])
+for _file in os.listdir('datacards/'):
+    if '.tgz' in _file: continue
+    if 'condor' in _file: continue
+    folder_list.append(_file.split('.')[0])
 
 txt_list = []
 root_list = []
@@ -18,17 +18,15 @@ for folder in folder_list:
         if '.txt' not in txt: continue
         txt_list.append('datacards/'+folder+'/'+txt)
     for root in os.listdir('datacards/'+folder+'/'):
-            if '.root' not in root: continue
-            root_list.append('datacards/'+folder+'/'+root)
-new_folder=options.analysis.replace('-','')
+        if '.root' not in root: continue
+        root_list.append('datacards/'+folder+'/'+root)
+new_folder=options.analysis.replace(':','')
 os.system('mkdir -p datacards/'+new_folder)
 os.system('rm datacards/'+new_folder+'/*')
 
 command='combineCards.py '
 for card in txt_list:
-    #if 'sr' not in card: continue
-    #if 'monojet' not in card: continue
-    if options.analysis+'-' not in card: continue
+    if not any(_analysis+'-' in card for _analysis in options.analysis.split(':')): continue
     filename=card.strip()
     print(filename)
     os.system('cp '+filename+' .')
@@ -38,6 +36,6 @@ command=command+' > datacards/'+new_folder+'/'+new_folder+'.txt'
 os.system(command)
 os.system('rm *.txt')
 for rootfile in root_list:
-    if options.analysis+'-' not in rootfile: continue
+    if not any(_analysis+'-' in rootfile for _analysis in options.analysis.split(':')): continue
     filename=rootfile.strip()
     os.system('cp '+filename+' datacards/'+new_folder)
