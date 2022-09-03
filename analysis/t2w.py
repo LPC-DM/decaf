383cee781e1866a933fc62e47d15d9dbafe074c6
Matteo Cremonesi <matteoc@fnal.gov>
2022-09-03 00:40:32 -0700

    Update t2w.py

--- analysis/t2w.py
+++ analysis/t2w.py
@@ -7,40 +7,24 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-s', '--signal', help='signal', dest='signal')
     parser.add_option('-f', '--folder', help='folder', dest='folder')
-    parser.add_option('--multiSignal', action='store_true', dest='multiSignal')
     (options, args) = parser.parse_args()
 
-    def multiSignalModel():
-        
-        signals=[]
-        for k,v in processes.items():
-            process = k
-            if not isinstance(k, str):
-                process = k[0]
-            if options.signal.split(':')[0] not in process: continue
-            if process not in signals:
-                signals.append(process)
+    '''
+    signals=[]
+    for k,v in processes.items():
+        process = k
+        if not isinstance(k, str):
+            process = k[0]
+        if options.signal.split(':')[0] not in process: continue
+        if process not in signals:
+            signals.append(process)
+    '''
+    option = ''
+    for signal in options.signal.split(','):
+        option += ' --PO map=.*/'+signal.split(:)[0]+':'+signal.split(:)[1]
     
-        tmp = []
-        name = ''
-        for signal in signals:
-            try:
-                if options.signal.split(':')[1] in signal:
-                    name='_'+signal
-                    label = ' --PO map=.*/'+signal+':'+signal+'_r[1,0,10]'
-                else:
-                    label = ' --PO map=.*/'+signal+':0'
-            except:
-                label = ' --PO map=.*/'+signal+':'+signal+'_r[1,0,10]'
-            tmp.append(label)
-        option = ''.join(tmp)
+    command = 'text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose'+option+' datacards/'+options.folder+'/'+options.folder+'.txt -o datacards/'+options.folder+'/'+options.folder+name+'.root'
+    return command
     
-        command = 'text2workspace.py -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose'+option+' datacards/'+options.folder+'/'+options.folder+'.txt -o datacards/'+options.folder+'/'+options.folder+name+'.root'
-        return command
-    
-    
-    command = 'text2workspace.py datacards/'+options.folder+'/'+options.folder+'.txt -o datacards/'+options.folder+'/'+options.folder+'.root'
-    if options.multiSignal: 
-        command = multiSignalModel()
     print(command)
     #os.system(command)
