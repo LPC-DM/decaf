c41eeb0815a813708f61e65d424837bb04207a20
Matteo Cremonesi <matteoc@fnal.gov>
2022-09-17 18:28:46 +0200

    Update doublebsf.py

--- analysis/models/doublebsf.py
+++ analysis/models/doublebsf.py
@@ -30,20 +30,7 @@ bbtagger_eff = {
         "2018": 0.945
         }
 
-#### New btagJP binnings for fit
-#new_bins = {
-#        "2016": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5],
-#        #"2016": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.5],
-#        "2017": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5],
-#        #"2018": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5],
-#        "2018": [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.5],
-#        }
-
-#### New sv mass binnings for fit
-new_bins = {
-        #"2016": [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,  0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2],
-        #"2017": [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,  0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2],
-        #"2018": [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,  0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2]
+binning = {
         "2016": [-0.8, -0.4, 0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2],
         "2017": [-0.8, -0.4, 0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2],
         "2018": [-0.8, -0.4, 0.,  0.1,  0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 2.5, 3.2]
@@ -52,35 +39,25 @@ new_bins = {
 ### category: pass/fail flag
 def template(dictionary, process, gentype, category, read_sumw2=False):
     histogram = dictionary[gentype].integrate("process", process)
-    jp, sumw2 = histogram.values(sumw2=True)[()]
-    jp = jp[:, category_map[category]]
+    nominal, sumw2 = histogram.values(sumw2=True)[()]
+    nominal = nominal[:, category_map[category]]
     sumw2 = sumw2[:, category_map[category]]
-    zerobins = jp <= 0.
-    output = jp
-
-    if 'QCD' in process:
+    zerobins = nominal <= 0.
+    output = nominal
+    if "data" not in gentype:
         output[zerobins] = 1e-5
         sumw2[zerobins] = 0.
-
-    #print(category, process, output)
-    binning = (
-        #dictionary[gentype].integrate("process", process).axis("btagJP").edges()
-        dictionary[gentype].integrate("process", process).axis("svmass").edges()
-    )
-    #print('Bin edges:', binning, '\n')
-
+    binning = (dictionary[gentype].integrate("process", process).axis("svmass").edges())
     if read_sumw2:
-        #return (output, binning, "btagJP", sumw2)
         return (output, binning, "svmass", sumw2)
-
     return (output, binning, "svmass")
 
-def get_mergedMC_stat_variations(dictionary, category, bkg_list):
-    templ=template(dictionary, 'QCD', bkg_list[0], category, read_sumw2=True)
+def get_mergedMC_stat_variations(dictionary, category, mc_list):
+    templ=template(dictionary, 'QCD', mc_list[0], category, read_sumw2=True)
     merged_central=np.zeros_like(templ[0])
     merged_error2=np.zeros_like(templ[3])
-    for bkg in bkg_list:
-        templ=template(dictionary, 'QCD', bkg, category, read_sumw2=True)
+    for mc in mc_list:
+        templ=template(dictionary, 'QCD', mc, category, read_sumw2=True)
         for i in range(len(templ[0])):
             if templ[0][i] <= 1e-5 or templ[3][i] <= 0.:
                 continue
@@ -105,7 +82,6 @@ def addBBliteSyst(templ, param, merged_central, merged_error2, epsilon=0):
         
 def model(year, category):
 
-    #model_id = "btagJP" + year + category
     model_id = "svmass" + year + category
     model = rl.Model(model_id)
 
@@ -123,88 +99,73 @@ def model(year, category):
     # Add data distribution to the channel
     ###
 
-    dataTemplate = template(data, "BTagMu", "bb", category)
+    dataTemplate = template(data, "BTagMu", "data", category)
     sr.setObservation(dataTemplate)
+    
+    ###
+    # Other MC processes
+    ###
+
     nbins = len(dataTemplate[1]) - 1
     param = [None for _ in range(nbins)]
     for i in range(nbins):
         param[i] = rl.NuisanceParameter(ch_name + '_mcstat_bin%i' % i, combinePrior='shape')
+        
+    mc_list = ['bb', 'b', 'cc', 'c', 'other']
+    total_yields, total_error2 = get_mergedMC_stat_variations(mc, category, mc_list)
 
     ###
     # QCD sig process
     ###
 
     ##### Template to use bb stat uncertainties
-    sr_genbb_Template = template(signal, "QCD", "bb", category, read_sumw2=True)
+    sr_genbb_Template = template(mc, "QCD", "bb", category, read_sumw2=True)
     sr_genbb = rl.TemplateSample(ch_name + "_genbb", rl.Sample.SIGNAL, sr_genbb_Template)
     sr_genbb.setParamEffect(lumi, nlumi)
     sr_genbb.setParamEffect(pu, npu)
     sr_genbb.setParamEffect(jes, njes)
     sr_genbb.setParamEffect(frac, nfrac)
     sr_genbb.setParamEffect(sf_weight, weight[category])
-    #sr_genbb.autoMCStats(shape=True)
-    #sr_genbb.autoMCStats(name=ch_name)
     addBBliteSyst(sr_genbb, param, total_yields, total_error2, epsilon=1e-5)
     sr.addSample(sr_genbb)
-    ###########################################
-
-    ##### Template without bb stat uncertainties
-    #sr_genbb_Template = template(signal, "QCD", "bb", category)
-    #sr_genbb_Observable = rl.Observable("btagJP", sr_genbb_Template[1])
-    #sr_genbb_BinYields = sr_genbb_Template[0] * weight[category]
-    #sr_genbb = rl.ParametericSample(ch_name + "_genbb", rl.Sample.SIGNAL, sr_genbb_Observable, sr_genbb_BinYields)
-    #sr_genbb.setParamEffect(lumi, 1.027)
-    #sr_genbb.setParamEffect(pu, 1.05)
-    #sr_genbb.setParamEffect(jes, 1.02)
-    #sr_genbb.setParamEffect(frac_bb, 1.5)
-    #sr.addSample(sr_genbb)
-    ###########################################
 
     ###
     # QCD bkg processes
     ###
 
-    sr_genb_Template = template(background, "QCD", "b", category, read_sumw2=True)
+    sr_genb_Template = template(mc, "QCD", "b", category, read_sumw2=True)
     sr_genb = rl.TemplateSample(ch_name + "_genb", rl.Sample.BACKGROUND, sr_genb_Template)
     sr_genb.setParamEffect(lumi, nlumi)
     sr_genb.setParamEffect(pu, npu)
     sr_genb.setParamEffect(jes, njes)
     sr_genb.setParamEffect(frac, nfrac)
-    #sr_genb.autoMCStats(shape=True)
-    #sr_genb.autoMCStats(name=ch_name)
     addBBliteSyst(sr_genb, param, total_yields, total_error2, epsilon=1e-5)
     sr.addSample(sr_genb)
 
-    sr_genc_Template = template(background, "QCD", "c", category, read_sumw2=True)
+    sr_genc_Template = template(mc, "QCD", "c", category, read_sumw2=True)
     sr_genc = rl.TemplateSample(ch_name + "_genc", rl.Sample.BACKGROUND, sr_genc_Template)
     sr_genc.setParamEffect(lumi, nlumi)
     sr_genc.setParamEffect(pu, npu)
     sr_genc.setParamEffect(jes, njes)
     sr_genc.setParamEffect(frac, nfrac)
-    #sr_genc.autoMCStats(shape=True)
-    #sr_genc.autoMCStats(name=ch_name)
     addBBliteSyst(sr_genc, param, total_yields, total_error2, epsilon=1e-5)
     sr.addSample(sr_genc)
 
-    sr_gencc_Template = template(background, "QCD", "cc", category, read_sumw2=True)
+    sr_gencc_Template = template(mc, "QCD", "cc", category, read_sumw2=True)
     sr_gencc = rl.TemplateSample(ch_name + "_gencc", rl.Sample.BACKGROUND, sr_gencc_Template)
     sr_gencc.setParamEffect(lumi, nlumi)
     sr_gencc.setParamEffect(pu, npu)
     sr_gencc.setParamEffect(jes, njes)
     sr_gencc.setParamEffect(frac, nfrac)
-    #sr_gencc.autoMCStats(shape=True)
-    #sr_gencc.autoMCStats(name=ch_name)
     addBBliteSyst(sr_gencc, param, total_yields, total_error2, epsilon=1e-5)
     sr.addSample(sr_gencc)
 
-    sr_genother_Template = template(background, "QCD", "other", category, read_sumw2=True)
+    sr_genother_Template = template(mc, "QCD", "other", category, read_sumw2=True)
     sr_genother = rl.TemplateSample(ch_name + "_genother", rl.Sample.BACKGROUND, sr_genother_Template)
     sr_genother.setParamEffect(lumi, nlumi)
     sr_genother.setParamEffect(pu, npu)
     sr_genother.setParamEffect(jes, njes)
     sr_genother.setParamEffect(frac, nfrac)
-    #sr_genother.autoMCStats(shape=True)
-    #sr_genother.autoMCStats(name=ch_name)
     addBBliteSyst(sr_genother, param, total_yields, total_error2, epsilon=1e-5)
     sr.addSample(sr_genother)
 
@@ -247,15 +208,6 @@ if __name__ == "__main__":
             "fail": rl.DependentParameter("weight", "(1-({0}*%f))/(1-%f)" % (bbtagger_eff[year], bbtagger_eff[year]), sf)
             }
     sf_weight = rl.IndependentParameter("sf_weight" + year, 1.0)
-    ############################################
-
-    #### SF weight (ParametericSample version) ####
-    #sf = rl.IndependentParameter("sf" + year, 1.0, 0.01, 1.0 / bbtagger_eff[year])
-    #weight = {
-    #        "pass": sf,
-    #        "fail": (1 - (sf*bbtagger_eff[year])) / (1 - bbtagger_eff[year])
-    #        }
-    ###############################################
 
     for category in ["pass", "fail"]:
 
@@ -278,28 +230,18 @@ if __name__ == "__main__":
         ###
         # Rebin templates for fit 
         ##
-        #data_hists["template"] = data_hists["template"].rebin("btagJP", hist.Bin("btagJP", "btagJP", new_bins[year]))
-        #bkg_hists["template"] = bkg_hists["template"].rebin("btagJP", hist.Bin("btagJP", "btagJP", new_bins[year]))
-        data_hists["svtemplate"] = data_hists["svtemplate"].rebin("svmass", hist.Bin("svmass", "svmass", new_bins[year]))
-        bkg_hists["svtemplate"] = bkg_hists["svtemplate"].rebin("svmass", hist.Bin("svmass", "svmass", new_bins[year]))
+        data_hists["svtemplate"] = data_hists["svtemplate"].rebin("svmass", hist.Bin("svmass", "svmass", binning[year]))
+        bkg_hists["svtemplate"] = bkg_hists["svtemplate"].rebin("svmass", hist.Bin("svmass", "svmass", binning[year]))
 
         ###
         # Preparing histograms for fit
         ##
         data = {}
-        #data['bb'] = data_hists["template"].sum("gentype", overflow='all')
-        data['bb'] = data_hists["svtemplate"].sum("gentype", overflow='all')
+        data['data'] = data_hists["svtemplate"].sum("gentype", overflow='all')
 
-        background = {}
-        signal = {}
-
+        mc = {}
         for i in range(5):
-            if gentype_map[i] == 'bb':
-                #signal[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", 0)
-                signal[str(gentype_map[i])] = bkg_hists["svtemplate"].integrate("gentype", 0)
-            else:
-                #background[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", i)
-                background[str(gentype_map[i])] = bkg_hists["svtemplate"].integrate("gentype", i)
+            mc[str(gentype_map[i])] = bkg_hists["svtemplate"].integrate("gentype", i)
 
         with open(
             "data/doublebSF-"
