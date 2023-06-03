eb15ae2cb981cf5f531db468ca978dc17e216b77
Matteo Cremonesi <matteoc@fnal.gov>
2023-06-03 19:39:50 -0400

    Update setup_lcg.sh

--- setup_lcg.sh
+++ setup_lcg.sh
@@ -3,7 +3,7 @@
 source env_lcg.sh
 
 pip install --user coffea==0.6.37
-pip install --user https://github.com/nsmith-/rhalphalib/archive/master.zip
+pip install --user https://github.com/mcremone/rhalphalib/archive/py3.zip
 pip install --user xxhash
 # progressbar, sliders, etc.
 jupyter nbextension enable --py widgetsnbextension
