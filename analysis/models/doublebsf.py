bc39766c3e63e8dec350ba65c655644971c2c8a4
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2023-07-18 12:27:35 -0500

    fixing model script

--- analysis/models/doublebsf.py
+++ analysis/models/doublebsf.py
@@ -45,12 +45,12 @@ def template(dictionary, process, gentype, category, pt, read_sumw2=False):
         return (output, binning, "svmass", sumw2)
     return (output, binning, "svmass")
 
-def get_mergedMC_stat_variations(dictionary, category, mc_list):
-    templ=template(dictionary, 'QCD', mc_list[0], category, read_sumw2=True)
+def get_mergedMC_stat_variations(dictionary, category, pt, mc_list):
+    templ=template(dictionary, 'QCD', mc_list[0], category, pt, read_sumw2=True)
     merged_central=np.zeros_like(templ[0])
     merged_error2=np.zeros_like(templ[3])
     for mc in mc_list:
-        templ=template(dictionary, 'QCD', mc, category, read_sumw2=True)
+        templ=template(dictionary, 'QCD', mc, category, pt, read_sumw2=True)
         for i in range(len(templ[0])):
             if templ[0][i] <= 1e-5 or templ[3][i] <= 0.:
                 continue
@@ -105,7 +105,7 @@ def model(year, category, pt):
         param[i] = rl.NuisanceParameter(ch_name + '_mcstat_bin%i' % i, combinePrior='shape')
         
     mc_list = ['bb', 'b', 'cc', 'c', 'other']
-    total_yields, total_error2 = get_mergedMC_stat_variations(mc, category, mc_list)
+    total_yields, total_error2 = get_mergedMC_stat_variations(mc, category, pt, mc_list)
 
     ###
     # QCD sig process
@@ -114,9 +114,9 @@ def model(year, category, pt):
     ##### Template to use bb stat uncertainties
     sr_genbb_Template = template(mc, "QCD", "bb", category, pt, read_sumw2=True)
     sr_genbb = rl.TemplateSample(ch_name + "_genbb", rl.Sample.SIGNAL, sr_genbb_Template)
-    sr_genbb.setParamEffect(lumi, nlumi)
-    sr_genbb.setParamEffect(pu, npu)
-    sr_genbb.setParamEffect(jes, njes)
+    sr_genbb.setParamEffect(lumi, 1.05)
+    sr_genbb.setParamEffect(pu, 1.02)
+    sr_genbb.setParamEffect(jes, 1.04)
     sr_genbb.setParamEffect(frac_b, 1.2)
     sr_genbb.setParamEffect(sf_weight['bb'], weight['bb'][category])
     addBBliteSyst(sr_genbb, param, total_yields, total_error2, epsilon=1e-5)
@@ -128,9 +128,9 @@ def model(year, category, pt):
 
     sr_genb_Template = template(mc, "QCD", "b", category, pt, read_sumw2=True)
     sr_genb = rl.TemplateSample(ch_name + "_genb", rl.Sample.BACKGROUND, sr_genb_Template)
-    sr_genb.setParamEffect(lumi, nlumi)
-    sr_genb.setParamEffect(pu, npu)
-    sr_genb.setParamEffect(jes, njes)
+    sr_genb.setParamEffect(lumi, 1.05)
+    sr_genb.setParamEffect(pu, 1.02)
+    sr_genb.setParamEffect(jes, 1.04)
     sr_genb.setParamEffect(frac_b, 1.2)
     sr_genbb.setParamEffect(sf_weight['b'], weight['b'][category])
     addBBliteSyst(sr_genb, param, total_yields, total_error2, epsilon=1e-5)
@@ -138,9 +138,9 @@ def model(year, category, pt):
 
     sr_genc_Template = template(mc, "QCD", "c", category, pt, read_sumw2=True)
     sr_genc = rl.TemplateSample(ch_name + "_genc", rl.Sample.BACKGROUND, sr_genc_Template)
-    sr_genc.setParamEffect(lumi, nlumi)
-    sr_genc.setParamEffect(pu, npu)
-    sr_genc.setParamEffect(jes, njes)
+    sr_genc.setParamEffect(lumi, 1.05)
+    sr_genc.setParamEffect(pu, 1.02)
+    sr_genc.setParamEffect(jes, 1.04)
     sr_genc.setParamEffect(frac_c, 1.2)
     sr_genbb.setParamEffect(sf_weight['c'], weight['c'][category])
     addBBliteSyst(sr_genc, param, total_yields, total_error2, epsilon=1e-5)
@@ -148,9 +148,9 @@ def model(year, category, pt):
 
     sr_gencc_Template = template(mc, "QCD", "cc", category, pt, read_sumw2=True)
     sr_gencc = rl.TemplateSample(ch_name + "_gencc", rl.Sample.BACKGROUND, sr_gencc_Template)
-    sr_gencc.setParamEffect(lumi, nlumi)
-    sr_gencc.setParamEffect(pu, npu)
-    sr_gencc.setParamEffect(jes, njes)
+    sr_gencc.setParamEffect(lumi, 1.05)
+    sr_gencc.setParamEffect(pu, 1.02)
+    sr_gencc.setParamEffect(jes, 1.04)
     sr_gencc.setParamEffect(frac_c, 1.2)
     sr_genbb.setParamEffect(sf_weight['cc'], weight['cc'][category])
     addBBliteSyst(sr_gencc, param, total_yields, total_error2, epsilon=1e-5)
@@ -158,9 +158,9 @@ def model(year, category, pt):
 
     sr_genother_Template = template(mc, "QCD", "other", category, pt, read_sumw2=True)
     sr_genother = rl.TemplateSample(ch_name + "_genother", rl.Sample.BACKGROUND, sr_genother_Template)
-    sr_genother.setParamEffect(lumi, nlumi)
-    sr_genother.setParamEffect(pu, npu)
-    sr_genother.setParamEffect(jes, njes)
+    sr_genother.setParamEffect(lumi, 1.05)
+    sr_genother.setParamEffect(pu, 1.02)
+    sr_genother.setParamEffect(jes, 1.04)
     sr_genother.setParamEffect(frac_other, 1.2)
     sr_genbb.setParamEffect(sf_weight['other'], weight['other'][category])
     addBBliteSyst(sr_genother, param, total_yields, total_error2, epsilon=1e-5)
@@ -180,7 +180,7 @@ if __name__ == "__main__":
     # Extract histograms from input file
     ###
 
-    print("Extracting histograms for", year, category)
+    #print("Extracting histograms for", year, category)
     hists = load("hists/doublebsf" + year + ".scaled")
     data_hists = hists["data"]
     bkg_hists = hists["bkg"]
@@ -189,8 +189,8 @@ if __name__ == "__main__":
     ###
     # Rebin templates for fit 
     ##
-    data_hists["template"] = data_hists["template"].rebin("fj1pt", hist.Bin("fj1pt", "fj1pt", binning[year]))
-    bkg_hists["template"] = bkg_hists["template"].rebin("fj1pt", hist.Bin("fj1pt", "fj1pt", binning[year]))
+    data_hists["template"] = data_hists["template"].rebin("fj1pt", hist.Bin("fj1pt", "fj1pt", pt_binning[year]))
+    bkg_hists["template"] = bkg_hists["template"].rebin("fj1pt", hist.Bin("fj1pt", "fj1pt", pt_binning[year]))
 
     ###
     # Preparing histograms for fit
@@ -203,18 +203,6 @@ if __name__ == "__main__":
         mc[str(gentype_map[i])] = bkg_hists["template"].integrate("gentype", i)
 
     ###
-    # Calculating efficiencies
-    ###
-
-    eff={}
-    for i in range(5):
-        num=mc[str(gentype_map[i])].integrate('svmass').integrate('process').integrate('ZHbbsQCD',1).values()[()]
-        den=mc[str(gentype_map[i])].integrate('svmass').integrate('process').integrate('ZHbbsQCD').values()[()]
-        eff[str(gentype_map[i])] = np.nan_to_num(num/den)
-
-        
-
-    ###
     ###
     # Setting up common systematics
     ###
@@ -224,28 +212,40 @@ if __name__ == "__main__":
     pu = rl.NuisanceParameter("pu" + year, "lnN")
     jes = rl.NuisanceParameter("jes" + year, "lnN")
     #qcd_norm = rl.NuisanceParameter("qcd_norm", "lnN")
-    frac_b = rl.NuisanceParameter("frac_b" + year + category, "lnN")
-    frac_c = rl.NuisanceParameter("frac_c" + year + category, "lnN")
-    frac_other = rl.NuisanceParameter("frac_other" + year + category, "lnN")
+    frac_b = rl.NuisanceParameter("frac_b" + year, "lnN")
+    frac_c = rl.NuisanceParameter("frac_c" + year, "lnN")
+    frac_other = rl.NuisanceParameter("frac_other" + year, "lnN")
 
     #### fractional systematics (assume 50%)
     #frac_bb = rl.NuisanceParameter("frac_bb" + year, "lnN")
 
-    #### SF weight (TemplateSample version) ####
-    sf={}
-    weight={}
-    sf_weight={}
-    for i in range(5):
-            sf[str(gentype_map[i])] = rl.IndependentParameter("sf"+ str(gentype_map[i]) + year, 1.0, 0.01, 1.0 / bbtagger_eff[year])
-            weight[str(gentype_map[i]) = {
-                    "pass": rl.DependentParameter("weight"+str(gentype_map[i]), "{0}", sf),
-                    "fail": rl.DependentParameter("weight"+str(gentype_map[i]), "(1-({0}*%f))/(1-%f)" % (eff[str(gentype_map[i])], eff[str(gentype_map[i])]), sf[str(gentype_map[i])])
-                    }
-            sf_weight[str(gentype_map[i])] = rl.IndependentParameter("sf_weight"+ str(gentype_map[i]) + year, 1.0)
-
-    ptbins = np.array(pt_binning)
+    ptbins = np.array(pt_binning[year])
     npt = len(ptbins) - 1
     for ptbin in range(npt):
+
+        ###
+        # Calculating efficiencies
+        ###
+
+        eff={}
+        for i in range(5):
+            num=mc[str(gentype_map[i])].integrate('svmass').integrate('process').values()[()][ptbin,1]
+            den=mc[str(gentype_map[i])].integrate('svmass').integrate('process').sum('ZHbbvsQCD').values()[()][ptbin]
+            eff[str(gentype_map[i])] = np.nan_to_num(num/den)
+
+
+        #### SF weight (TemplateSample version) ####
+        sf={}
+        weight={}
+        sf_weight={}
+        for i in range(5):
+            sf[str(gentype_map[i])] = rl.IndependentParameter("sf"+ str(gentype_map[i]) + year, 1.0, 0.01, 1.0 / eff[str(gentype_map[i])])
+            weight[str(gentype_map[i])] = {
+                "pass": rl.DependentParameter("weight"+str(gentype_map[i]), "{0}", sf[str(gentype_map[i])]),
+                "fail": rl.DependentParameter("weight"+str(gentype_map[i]), "(1-({0}*%f))/(1-%f)" % (eff[str(gentype_map[i])], eff[str(gentype_map[i])]), sf[str(gentype_map[i])])
+            }
+            sf_weight[str(gentype_map[i])] = rl.IndependentParameter("sf_weight"+ str(gentype_map[i]) + year, 1.0)
+        
         for category in ["pass", "fail"]:
 
                 with open(
@@ -258,4 +258,4 @@ if __name__ == "__main__":
                     + ".model",
                     "wb",
         ) as fout:
-            pickle.dump(model(year, category, ptbin), fout, protocol=2)
+                    pickle.dump(model(year, category, ptbin), fout, protocol=2)
