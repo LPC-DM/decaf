77b191b4f089defdbff264470f77cbebab7b5267
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2023-09-23 08:40:09 +0900

    btag syst hopefully fixed

---
+++ analysis/macros/run_darkhiggs.sh
@@ -0,0 +1,3 @@
+python run_condor.py -p darkhiggs2018 -m full_2018 -d mhs -e mhs150 -c kisti -t -x
+python run_condor.py -p darkhiggs2017 -m full_2017 -d mhs -e mhs150 -c kisti 
+python run_condor.py -p darkhiggs2016 -m full_2016 -e BTag,GJets,mhs150,100To200,SinglePhoton,100to200 -c kisti
