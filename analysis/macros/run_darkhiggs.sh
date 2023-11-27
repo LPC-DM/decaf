bfcf3ee0c5666a8cff4659c48f7e79be8ecaad94
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2023-11-28 08:20:15 +0900

    new plots

--- analysis/macros/run_darkhiggs.sh
+++ analysis/macros/run_darkhiggs.sh
@@ -1,3 +1,1 @@
-python run_condor.py -p darkhiggs2018 -m full_2018 -d mhs -c kisti -t -x
-python run_condor.py -p darkhiggs2017 -m full_2017 -d mhs -c kisti
 python run_condor.py -p darkhiggs2016 -m full_2016 -d mhs -c kisti
