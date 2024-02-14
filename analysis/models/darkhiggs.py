f9b789eae6774f6638bb5c555932306d6e47e24a
Matteo Cremonesi <matteoc@fnal.gov>
2024-02-14 02:21:18 -0500

    Update darkhiggs.py

--- analysis/models/darkhiggs.py
+++ analysis/models/darkhiggs.py
@@ -174,7 +174,8 @@ class TransferFactorSample(rl.ParametericSample):
         else:
             raise ValueError("Transfer factor has invalid dimension")
         #super(TransferFactorSample, self).__init__(samplename, sampletype, observable, params)
-        rl.ParametericSample.__init__(self, samplename, sampletype, observable, params)
+        #rl.ParametericSample.__init__(self, samplename, sampletype, observable, params)
+        super().__init__(samplename, sampletype, observable, params)
         self._transferfactor = transferfactor
         self._dependentsample = dependentsample
         self._nominal = nominal
