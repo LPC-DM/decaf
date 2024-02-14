ba4f41b98b2a9e3496a7519953375395b50cdbef
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2024-02-15 05:52:39 +0900

    almost works

--- analysis/models/darkhiggs.py
+++ analysis/models/darkhiggs.py
@@ -5,11 +5,11 @@ import concurrent.futures
 import sys
 import os
 import rhalphalib as rl
+from libs.myrhalphalib import TransferFactorSample
 import numpy as np
 import pickle
 import gzip
 import json
-import uncertainties as unc  
 import uncertainties.unumpy as unumpy 
 from coffea import hist, processor
 from coffea.util import load, save
@@ -119,74 +119,6 @@ def remap_histograms(hists):
 
     return hists
 
-class TransferFactorSample(rl.ParametericSample):
-    def __init__(self, samplename, sampletype, transferfactor, dependentsample, nominal_values=None, stat_unc=None, observable=None, min_val=None, epsilon=1e-5, effect_threshold=0.01, channel_name=None):
-        """
-        Create a sample that depends on another Sample by some transfer factor.
-        The transfor factor can be a constant, an array of parameters of same length
-        as the dependent sample binning, or a matrix of parameters where the second
-        dimension matches the sample binning, i.e. expectation = tf @ dependent_expectation.
-        The latter requires an additional observable argument to specify the definition of the first dimension.
-        In all cases, please use numpy object arrays of Parameter types.
-        Passing in a ``min_val`` means param values will be clipped at the min_val.
-        """
-        if not isinstance(transferfactor, np.ndarray):
-            raise ValueError("Transfer factor is not a numpy array")
-        if not isinstance(dependentsample, rl.Sample):
-            raise ValueError("Dependent sample does not inherit from Sample")
-        if len(transferfactor.shape) == 2:
-            if observable is None:
-                raise ValueError("Transfer factor is 2D array, please provide an observable")
-            params = np.dot(transferfactor, dependentsample.getExpectation())
-            if min_val is not None:
-                for idx, p in np.ndenumerate(params):
-                    params[idx] = p.max(min_val)
-        elif len(transferfactor.shape) <= 1:
-            observable = dependentsample.observable
-            if stat_unc is not None:
-                name = samplename if channel_name is None else channel_name
-                MCStatTemplate = (np.ones_like(unc), observable._binning, observable._name)
-                MCStat = rl.TemplateSample(name+'_mcstat', rl.Sample.BACKGROUND, MCStatTemplate)
-                for i in range(MCStat.observable.nbins):
-                    effect_up = np.ones_like(MCStat._nominal)
-                    effect_down = np.ones_like(MCStat._nominal)
-                    if stat_unc < effect_threshold:
-                        continue
-                    effect_up[i] = 1.0 + min(1.0, stat_unc)
-                    effect_down[i] = max(epsilon, 1.0 - min(1.0, stat_unc))
-                    print(samplname, i, ntot, effect_up[i], effect_down[i])
-                    param = rl.NuisanceParameter(name + '_mcstat_bin%i' % i, combinePrior='shape')
-                    MCStat.setParamEffect(param, effect_up, effect_down)
-                params = transferfactor * MCStat.getExpectation() * dependentsample.getExpectation()
-            else:
-                params = transferfactor * dependentsample.getExpectation()
-            if min_val is not None:
-                for i, p in enumerate(params):
-                    params[i] = p.max(min_val)
-        else:
-            raise ValueError("Transfer factor has invalid dimension")
-        super(TransferFactorSample, self).__init__(samplename, sampletype, observable, params)
-        self._transferfactor = transferfactor
-        self._dependentsample = dependentsample
-        self._stat_unc = stat_unc
-        self._nominal_values = nominal_values
-
-    @property
-    def transferfactor(self):
-        return self._transferfactor
-
-    @property
-    def dependentsample(self):
-        return self._dependentsample
-
-    @property
-    def stat_unc(self):
-        return self._stat_unc
-
-    @property
-    def nominal_values(self):
-        return self._nominal_values
-
 def makeTF(num, den):
 
     tf = num.getExpectation()/den.getExpectation()
@@ -220,18 +152,20 @@ def addBBLiteSyst(channel, epsilon=1e-5, effect_threshold=0.01, threshold=0, inc
         if isinstance(sample, TransferFactorSample):
             ntot += sample._nominal_values
             etot2 += (sample._stat_unc*sample._nominal_values)**2
-        else:
+        elif isinstance(sample, rl.TemplateSample):
             ntot += sample._nominal
             etot2 += sample._sumw2
-        if not include_signal and sample._sampletype == rl.Sample.SIGNAL:
+            if not include_signal and sample._sampletype == rl.Sample.SIGNAL:
+                continue
+            ntot_bb += sample._nominal
+            etot2_bb += sample._sumw2
+        else:
             continue
-        ntot_bb += sample._nominal
-        etot2_bb += sample._sumw2
 
     for sample in channel._samples.values():
         if not isinstance(sample, TransferFactorSample):
             continue
-        channel._samples[sample.name] = TransferFactorSample(sample.name, rl.Sample.BACKGROUND, sample.transferfactor, sample.dependentsample, nominal_values=sample.nominal_values, sumw2=np.sqrt(etot2)/ntot, channel_name=name)
+        channel._samples[sample.name] = TransferFactorSample(sample.name, rl.Sample.BACKGROUND, sample.transferfactor, sample.dependentsample, nominal_values=sample.nominal_values, stat_unc=np.sqrt(etot2)/ntot, channel_name=name)
             
         
     for i in range(first_sample.observable.nbins):
@@ -264,9 +198,9 @@ def addBBLiteSyst(channel, epsilon=1e-5, effect_threshold=0.01, threshold=0, inc
             param = rl.NuisanceParameter(name + "_mcstat_bin%i" % i, combinePrior="shape")
     
             for sample in channel._samples.values():
-                if sample._nominal[i] <= 1e-5:
-                    continue
                 if not isinstance(sample, rl.TemplateSample):
+                    continue
+                if sample._nominal[i] <= 1e-5:
                     continue
                 print(sample._name, i, sample._nominal[i], effect_up[i], effect_down[i])
                 sample.setParamEffect(param, effect_up, effect_down)
@@ -1562,11 +1496,11 @@ if __name__ == "__main__":
         addVJetsSyst(background, recoilbin, "W+jets", "sr", sr_wjetsMCFail, "fail")
 
         tf, unc = makeTF(sr_wjetsMCFail, sr_zjetsMCFail)
-        sr_wjetsFail = rl.TransferFactorSample(
+        sr_wjetsFail = TransferFactorSample(
             "sr" + year + "fail" + "mass" + mass + "recoil" + str(recoilbin) + "_wjets",
             rl.Sample.BACKGROUND,
             tf,
-            sr_zjetsFail)#,
+            sr_zjetsFail,
             nominal_values=sr_wjetsMCFail._nominal,
             stat_unc=unc
         )
